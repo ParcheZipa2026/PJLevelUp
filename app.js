1cf671603a174ef9f5a164ce6dbbbf12c90bccdb
ParcheZipa2026 <familia.cont6@zipaquira.gov.co>
2026-09-12 23:23:15 -0500

    Update APPS_SCRIPT_URL to new endpoint

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
 const CONFIG = {
   // Pega aquí la URL de tu Web App de Apps Script (ver README para el paso a paso)
-  APPS_SCRIPT_URL: "https://script.google.com/macros/library/d/1V6Wg9LWlxeaYPr1DJVpRj0Z-0gyXyfwM0hgGtq-ibRo3Bdbh6EDGq-rx/3",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxHeJ3FPjWNRSbR6z0JLrrbl4tZ2AfU930wuETKU5PCqykhPPSwk0q1pG8BeOjQuVr0/exec",
   // Fecha y hora límite de votación (hora de Colombia)
   DEADLINE: new Date("2026-09-20T23:59:59-05:00")
 };
